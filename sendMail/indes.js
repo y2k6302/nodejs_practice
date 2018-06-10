c086143246eb424cc7d503da353d996cd5e646a6
ianchen <y2k6302@gmail.com>
2018-06-10 14:40:20 +0800

    add new project
    send mail

---
+++ sendMail/indes.js
@@ -0,0 +1,25 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'account@gmail.com',
+    pass: 'password'
+  }
+});
+
+
+var mailOptions = {
+    from: 'from@gmail.com',
+    to: 'to@hotmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
